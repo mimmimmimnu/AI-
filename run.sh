a2299e9bc0c58fe7220f4567902dd4122e722ada
jennifer07145-source <jennifer07145@gmail.com>
2026-06-03 16:54:25 +0900

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-flutter pub get
-flutter run -d chrome --web-port=8080 \
-  --dart-define=GROQ_API_KEY=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 \
-  --dart-define=NAVER_CLIENT_ID=vBNJQC1zTEGhrFTUMYDm \
-  --dart-define=NAVER_CLIENT_SECRET=bzG4DPc5cu \
-  --web-browser-flag="--disable-web-security"
